653dfb1f80304a29cdaa2e18a6801cae40cf8df2
GoodGD <dionatha.work@gmail.com>
2025-09-30 16:20:10 -0300

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,35 +2,46 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
+export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "coverage/**",
+      "cypress/**",
+      "*.cjs",
+      "*.mjs",
+      "test-results.json"
+    ],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
+      ...pluginReact.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": "off",
       "no-empty": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
-
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
-]);
+];
